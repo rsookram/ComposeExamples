14e4a134300e4c288e6c8174d6f45f2be4ee1573
rsookram <rashadsookram@gmail.com>
2020-08-31 11:55:38 -0400

    Add example for handling fontScale changes

--- app/src/main/java/io/github/rsookram/compose/example/MainActivity.kt
+++ app/src/main/java/io/github/rsookram/compose/example/MainActivity.kt
@@ -3,10 +3,17 @@ package io.github.rsookram.compose.example
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.compose.foundation.Box
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
 import androidx.ui.tooling.preview.Preview
 import io.github.rsookram.compose.example.ui.AppTheme
@@ -17,10 +24,34 @@ class MainActivity : ComponentActivity() {
 
         Log.d("MainActivity", "onCreate called for activity $this")
 
+        val examples: List<@Composable () -> Unit> = listOf(
+            { FontScale() }
+        )
+
+        val currentIndex = mutableStateOf(0)
+
         setContent {
             AppTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    val onNextClick: (() -> Unit)? = if (currentIndex.value < examples.lastIndex) {
+                        { currentIndex.value++ }
+                    } else {
+                        null
+                    }
+                    val onPreviousClick: (() -> Unit)? = if (currentIndex.value > 0) {
+                        { currentIndex.value-- }
+                    } else {
+                        null
+                    }
+
+                    ExampleContainer(
+                        currentIndex = currentIndex.value + 1,
+                        totalCount = examples.size,
+                        onNextClick = onNextClick,
+                        onPreviousClick = onPreviousClick,
+                    ) {
+                        examples[currentIndex.value]()
+                    }
                 }
             }
         }
@@ -28,14 +59,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun FontScale() {
+    Text("Configuration Changes in Compose")
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    AppTheme {
-        Greeting("Android")
+fun FontScalePreview() = AppTheme {
+    FontScale()
+}
+
+@Composable
+fun ExampleContainer(
+    currentIndex: Int,
+    totalCount: Int,
+    onNextClick: (() -> Unit)?,
+    onPreviousClick: (() -> Unit)?,
+    child: @Composable () -> Unit,
+) {
+    Column {
+        Box(Modifier.weight(1f), children = child)
+
+        Row {
+            if (onPreviousClick != null) {
+                Button(onClick = onPreviousClick) { Text("Previous") }
+            } else {
+                Spacer(Modifier.weight(1f))
+            }
+
+            Text("$currentIndex / $totalCount")
+
+            if (onNextClick != null) {
+                Button(onClick = onNextClick) { Text("Next") }
+            } else {
+                Spacer(Modifier.weight(1f))
+            }
+        }
     }
 }
