0302d2a029a9f6850f9c6aecb93ca45d641e512b
rsookram <rashadsookram@gmail.com>
2020-09-01 11:39:58 -0400

    Add example for handling locale changes

--- app/src/main/java/io/github/rsookram/compose/example/MainActivity.kt
+++ app/src/main/java/io/github/rsookram/compose/example/MainActivity.kt
@@ -1,8 +1,10 @@
 package io.github.rsookram.compose.example
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Box
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.Column
@@ -13,7 +15,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ConfigurationAmbient
+import androidx.compose.ui.platform.ContextAmbient
 import androidx.compose.ui.platform.setContent
 import androidx.ui.tooling.preview.Preview
 import io.github.rsookram.compose.example.ui.AppTheme
@@ -25,7 +30,8 @@ class MainActivity : ComponentActivity() {
         Log.d("MainActivity", "onCreate called for activity $this")
 
         val examples: List<@Composable () -> Unit> = listOf(
-            { FontScale() }
+            { FontScale() },
+            { Locale() },
         )
 
         val currentIndex = mutableStateOf(0)
@@ -63,10 +69,35 @@ fun FontScale() {
     Text("Configuration Changes in Compose")
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 fun FontScalePreview() = AppTheme {
     FontScale()
+}
+
+@Composable
+fun Locale() {
+    Text(localizedString(R.string.android))
+}
+
+/**
+ * Replacement for [androidx.compose.ui.res.stringResource] which handles
+ * recomposing on [Configuration] changes (`stringResource` doesn't as of
+ * 1.0.0-alpha01).
+ *
+ * https://issuetracker.google.com/issues/167352819
+ */
+@Composable
+fun localizedString(@StringRes id: Int): String {
+    val context = ContextAmbient.current
+    val resources = remember(ConfigurationAmbient.current) { context.resources }
+    return resources.getString(id)
+}
+
+@Preview
+@Composable
+fun LocalePreview() = AppTheme {
+    Locale()
 }
 
 @Composable
