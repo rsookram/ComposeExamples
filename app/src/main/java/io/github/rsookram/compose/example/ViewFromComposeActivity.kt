9c9121b32c912493306e6c9dd9e682bb718906e1
rsookram <rashadsookram@gmail.com>
2020-10-23 15:50:42 -0400

    Add ViewBinding example

--- app/src/main/java/io/github/rsookram/compose/example/ViewFromComposeActivity.kt
+++ app/src/main/java/io/github/rsookram/compose/example/ViewFromComposeActivity.kt
@@ -1,11 +1,14 @@
 package io.github.rsookram.compose.example
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.viewinterop.AndroidViewBinding
+import io.github.rsookram.compose.example.databinding.ViewBindingExampleBinding
 
 class ViewFromComposeActivity : ComponentActivity() {
 
@@ -16,6 +19,12 @@ class ViewFromComposeActivity : ComponentActivity() {
             Column {
                 AndroidView(::TextView) { v ->
                     v.text = "TextView from Compose"
+                }
+
+                AndroidViewBinding(ViewBindingExampleBinding::inflate) {
+                    startText.setOnClickListener {
+                        Log.d("ViewFromComposeActivity", "start text clicked")
+                    }
                 }
             }
         }
